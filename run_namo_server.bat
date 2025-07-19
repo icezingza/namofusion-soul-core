7cb7f26113ac519c4248dd36737fa263fc025776
icezingza <icezingza@gmail.com>
2025-07-19 07:46:13 +0700

    Update run_namo_server.bat

--- run_namo_server.bat
+++ run_namo_server.bat
@@ -1,6 +1,5 @@
 @echo off
-echo Starting NaMo Framework API Server...
-cd /d %~dp0
-venv\Scripts\activate
-uvicorn main:app --reload --host 0.0.0.0 --port 8000
+cd /d "%~dp0"
+python -m pip install -r requirements.txt
+uvicorn main:app --reload
 pause
